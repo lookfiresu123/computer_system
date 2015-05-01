65b33c340d3776a0ac0c255f59fbc0c0ee18ea24
lookfiresu123 <804305986@qq.com>
2015-05-01 18:35:33 +0800

    修改了little_endian.c文件

---
+++ chapter2/little_endian.c
@@ -0,0 +1,46 @@
+/* check for little endian */
+/* 某个类型的指针实际上是一组连续的字节序列（长度待定） */
+
+#include<stdio.h>
+
+void self(){
+	int i;
+	printf("input a hexadecimal\n");
+	scanf("%x",&i);
+	char *p = NULL;
+	p = &i;
+	printf("%x ----> %x\n",p,*p);
+	printf("%x ----> %x\n",p+1,*(p+1));
+	printf("%x ----> %x\n",p+2,*(p+2));
+	printf("%x ----> %x\n",p+3,*(p+3));
+}
+
+typedef unsigned char *byte_pointer;//指向一个char的指针等于指向一个byte的指针
+
+void show_bytes(byte_pointer start,int len){
+	int i;
+	for(i=0;i<len;i++)
+		printf("%.2x",start[i]);
+	printf("\n");
+}
+
+void show_int(int x){
+	show_bytes((byte_pointer)&x,sizeof(int));
+}
+
+void show_float(float x){
+	show_bytes((byte_pointer)&x,sizeof(float));
+}
+
+void show_pointer(void *x){
+	show_bytes((byte_pointer)&x,sizeof(void *));
+}
+
+void main(){
+	void *p = malloc(sizeof(void *));
+	show_int(1);
+	show_float(1);
+	show_pointer(p);
+	printf("%.2x\n",p);
+}
+
