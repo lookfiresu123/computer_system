847c0bf45df6e573404e7350b3a88eebe15772c2
lookfiresu123 <804305986@qq.com>
2015-05-11 22:31:16 +0800

    修改union_intersection.c文件，修复bug

--- chapter2/union_intersection.c
+++ chapter2/union_intersection.c
@@ -10,19 +10,19 @@ int rgb_len = 3;//默认值
 
 
 
-typedef struct {
+typedef struct COLOR_RGB{
 	//int rgb_len;
 	//int rgb[rgb_len];
 	int *rgb;
 	char *color;
 } COLOR_RGB;   //三原色结构体
 
-typedef struct {
+typedef struct Node{
 	COLOR_RGB *content;
 	struct Node *next;
 }Node;
 
-typedef struct {
+typedef struct LENGTH{
 	int LEN_color;//color的长度
 	int bitlen;//位的长度
 } LENGTH;    //长度
@@ -43,25 +43,20 @@ void Union_intersection(){
 	length->bitlen = 0;
 	COLOR_RGB *color_rgb;
 	Node *current,*head;
-	head = current;//将current的初始地址保存在head里
-	//Node node;
-	//list->head = list->current = list->next = NULL;
+	head = (Node *)malloc(sizeof(Node));
+	current = head;//将head的初始地址保存在current里,head为头节点，内容为空，第一个颜色的节点存在head->next中
 	COLOR_RGB *buffer;
 	int i;
-	//printf("%d\n",strlen(color));
-	//printf("%s\n",color[7]);
 	M_LEN_COLOR(color,length->LEN_color);
 	int temp = length->LEN_color;
 	M_LEN_bit(temp,length->bitlen);
-	//printf("%d,%d\n",length->LEN_color,length->bitlen);
+	Node node[length->LEN_color];
 	for(i=0;i<length->LEN_color;i++){
-		Node *node = (Node *)malloc(sizeof(Node));
-		current = node;
+		current->next = &node[i];		
+		current = current->next;		
 		color_rgb = (COLOR_RGB *)malloc(sizeof(COLOR_RGB));
 		color_rgb->color = color[i];
-		//color_rgb->rgb_len = length->bitlen;
 		rgb_len = length->bitlen;
-		//color_rgb = (COLOR_RGB *)malloc(sizeof(COLOR_RGB));
 		int j;
 		int k = i;
 		int rgb_temp[length->LEN_color][rgb_len];
@@ -69,19 +64,17 @@ void Union_intersection(){
 			rgb_temp[i][j] = k % 2;
 			k = k / 2;
 		}
-		color_rgb->rgb = rgb_temp[i];
-		//list->head = color_rgb;
+		color_rgb->rgb = (int *)malloc(rgb_len*sizeof(int));
+		for(j=0;j<rgb_len;j++){
+			*(color_rgb->rgb+j) = rgb_temp[i][j];
+		}
 		current->content = color_rgb;
-		current->next = NULL;
-		printf("%s : %d%d%d\n",current->content->color,*(current->content->rgb),*(current->content->rgb+1),*(current->content->rgb+2));
-		current = current->next;
-		
+		current->next = NULL;		
 	}
-	current = head;
-	//printf("%s\n",current->content->color);
-	// while(current){
-		// printf("%s : %d%d%d\n",current->content->color,*(current->content->rgb),*(current->content->rgb+1),*(current->content->rgb+2));
-		// current = current->next;
-	// }
+	current = head->next;
+	while(current){
+		printf("%s : %d %d %d\n",current->content->color,*(current->content->rgb),*(current->content->rgb+1),*(current->content->rgb+2));
+		current = current->next;
+	}
 }
 
