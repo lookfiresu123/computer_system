d830db2416f8f0c108166accbe5d74405feb89b7
lookfiresu <804305986@qq.com>
2015-05-18 21:51:23 +0800

    增加了w位缩短到w-k位的unsigned类型算法

--- chapter2/header/shift_and_switch.h
+++ chapter2/header/shift_and_switch.h
@@ -2,4 +2,5 @@
 
 int fun1(unsigned word);
 int fun2(unsigned word);
+unsigned short unsigned_32bit_to_16bit(unsigned int x);
 void test_shift_and_switch();
--- chapter2/source/shift_and_switch.c
+++ chapter2/source/shift_and_switch.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 
 int fun1(unsigned word){
     return (int) ((word << 24) >> 24);
@@ -8,10 +9,18 @@ int fun2(unsigned word){
     return (((int)word << 24) >> 24);
 }
 
+unsigned short unsigned_32bit_to_16bit(unsigned int x){
+    unsigned short y = x % (unsigned int)pow(2,16);//unsigned x for w bits --> for w-k bits <==> unsigned x mod pow(2,k)
+    return y;
+}
+
 void test_shift_and_switch(){
     printf("input a unsigned word\n");
     unsigned word;
-    scanf("%x",&word);//十六进制%x都是unsigned类型
-    printf("%x执行fun1后得：%x或%d\n",word,fun1(word),fun1(word));
-    printf("%x执行fun2后得：%x或%d\n",word,fun2(word),fun2(word));
+    unsigned int x;
+    //scanf("%x",&word);//十六进制%x都是unsigned类型
+    //printf("%x执行fun1后得：%x或%d\n",word,fun1(word),fun1(word));
+    //printf("%x执行fun2后得：%x或%d\n",word,fun2(word),fun2(word));
+    scanf("%x",&x);
+    printf("%x执行unsigned_32bit_to_16bit后得： %hx或%hu\n",x,unsigned_32bit_to_16bit(x),unsigned_32bit_to_16bit(x));
 }
